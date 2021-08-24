41cc397f2c44cbe8ad87f2df0f0c044490709228
arjun-the-cool <80453159+arjun-the-cool@users.noreply.github.com>
2021-08-24 15:59:08 +0530

    Update login.js

--- login.js
+++ login.js
@@ -5,4 +5,6 @@ function addUser()
 
     localStorage.setItem("player1_name", player1_name);
     localStorage.setItem("player2_name", player2_name);
+    
+    window.location = "gamepage.html";
 }
